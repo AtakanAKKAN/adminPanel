6f19d4e0c6c657ecd7d6663491ab6b239cdb87de
Atakan Akkan <85180975+AtakanAKKAN@users.noreply.github.com>
2022-12-24 18:17:15 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const body = document.querySelector("body");
      sidebar = body.querySelector("nav");
      sidebarToggle = body.querySelector(".sidebar-toggle");
 
+let getMode= localStorage.getItem("mode");
+if(getMode && getMode === "dark"){
+    body.classList.toggle("dark")
+}     
 
 modeToggle.addEventListener("click", () => {
     body.classList.toggle("dark");
+    if(body.classList.contains("dark")){
+        localStorage.setItem("mode", "dark")
+    }else{
+        localStorage.setItem("mode", "light")
+    }
 });
 
 sidebarToggle.addEventListener("click", () => {
